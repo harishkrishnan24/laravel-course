c7b392bd8ac49512daf4d47e63f18f10c2210a8a
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-08-14 04:36:01 +0300

    Show comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,4 +9,12 @@
     @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 5 )
         <strong>New!</strong>
     @endif
+
+    <h4>Comments</h4>
+    @forelse ($post->comments as $comment)
+        <p>{{ $comment->content }}</p>
+        <p class="text-muted">added {{ $comment->created_at->diffForHumans()}}</p>
+    @empty
+        <p>No Comments yet!</p>
+    @endforelse
 @endsection('content')
